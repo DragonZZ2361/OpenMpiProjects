6061c06c2a4cfffb3554e492d5123164237da9f2
DragonZZ <1451590580@qq.com>
2023-04-18 10:26:44 +0800

    Modify HelloWorld

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,14 +1,21 @@
 #include <mpi.h>
-#include <iostream>
+#include <string.h>
+#include <string>
 
 int main(int argc, char** argv){
+    char message[20];
+    int myrank;
+    MPI_Status status;
     MPI_Init(&argc, &argv);
-    int world_size, world_rank;
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-
-    std::cout << "Current Process : " << world_rank \
-    << "\tTotal Process : " << world_size << std::endl;
-
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+    if(myrank == 0){
+        strcpy(message, "Hello, there");
+        MPI_Send(message, strlen(message) + 1, MPI_CHAR, 1 , 99, MPI_COMM_WORLD);
+    }
+    else if(myrank == 1){
+        MPI_Recv(message, 20, MPI_CHAR, 0, 99, MPI_COMM_WORLD, &status);
+        printf("received :%s: \n", message);
+    }
     MPI_Finalize();
+    return 0;
 }
